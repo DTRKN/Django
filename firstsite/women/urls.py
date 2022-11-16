862a80c50526ff24d55fd614bdfbb64972bda893
DTRKN <taratorkin.den@mail.ru>
2022-11-16 14:51:28 +0500

    sample

--- firstsite/women/urls.py
+++ firstsite/women/urls.py
@@ -4,6 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index), 
-    path('cats/<int:cat_id>/', categories), #http://127.0.0.1:8000/cats/1/
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),    
 ]
--- firstsite/women/views.py
+++ firstsite/women/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from .models import *
+
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
 def index(request):
-    return HttpResponse("Страница приложения women.")
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 def categories(request, cat_id):
     if(request.POST):
