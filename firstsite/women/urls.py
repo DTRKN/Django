901cc317ec91a23e93b1605d4a99884affce5ac0
DTRKN <taratorkin.den@mail.ru>
2022-11-18 18:52:58 +0500

    url

--- firstsite/women/urls.py
+++ firstsite/women/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index), 
+    path('', index, name='home'), 
     path('about/', about, name='about'),    
 ]
