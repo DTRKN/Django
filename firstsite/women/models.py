1ecf29e6c7c39a8ad4a23ddfbdb2f5ad1a133ca3
DTRKN <taratorkin.den@mail.ru>
2022-11-22 19:16:11 +0500

    url

--- firstsite/women/models.py
+++ firstsite/women/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
@@ -11,3 +11,6 @@ class Women(models.Model):
     #python manage.py shell
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
--- firstsite/women/urls.py
+++ firstsite/women/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'), 
-    path('about/', about, name='about'),    
+    path('home/', index, name='home'), 
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contant/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),   
 ]
