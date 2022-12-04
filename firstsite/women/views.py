1bcae393c15f185aeb6a5bc04fff93fef960bd19
DTRKN <taratorkin.den@mail.ru>
2022-12-05 03:23:40 +0500

    add content

--- firstsite/women/views.py
+++ firstsite/women/views.py
@@ -1,7 +1,10 @@
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+
+from .forms import *
 from .models import *
+
 
 menu = [{'title':'O сайте', 'url_name': 'about'},
         {'title':'Добавить статью', 'url_name': 'add_page'},
@@ -25,7 +28,19 @@ def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 def addpage(request):
-    return render(request, 'women/addpage.html', {'menu': menu, 'title': 'Добавление статьи'})
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            #print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+
+    else:   
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 def contact(request):
     return HttpResponse('Обратная связь')
